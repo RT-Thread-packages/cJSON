ff8690e33a384f4ada855ca3b180320f8711d206
fulingzhong <fulingzhong@163.com>
2018-02-22 09:52:43 +0800

    add cJSON_getJsonItem function， which is convenient to get cJSON object

--- cJSON_util.h
+++ cJSON_util.h
@@ -17,7 +17,7 @@ extern void cJSON_free(void *ptr);
 extern const char * cJSON_item_get_string(cJSON *object,const char *string);
 extern int cJSON_item_get_number(cJSON *object, const char *item_name, int * result);
 extern void cJSON_AddInteger2StringToObject(cJSON *object, const char *name, int i);
-
+extern int cJSON_getJsonItem(const cJSON* SrcItem, const char* path, cJSON** ResultItem);
 #ifdef __cplusplus
 }
 #endif
